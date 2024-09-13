d7d27daff4e0055105b0157259a9b6ebe88c72e3
rachaelritter <144731888+rachaelritter@users.noreply.github.com>
2024-09-12 23:48:16 -0400

    Delete Python directory

--- Python/hello.py
+++
@@ -1,6 +0,0 @@
-print ("hello world")
-
-firstNumber = int(input ("Please enter first number: "))
-secondNumber = int(input ("Please enter second number: "))
-myNumber = firstNumber + secondNumber
-print (myNumber)
--- Python/main.py
+++
@@ -1,2 +0,0 @@
-print("I'm learning how to program in Python.")
-print("That's awesome!")
