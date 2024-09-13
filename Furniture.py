cb975554bfe46a6940854842a3ff05b33baa2012
Rachael Ritter <rachaelritter@Rachaels-MBP.lan>
2024-09-12 23:36:28 -0400

    first commit

---
+++ Furniture.py
@@ -0,0 +1,16 @@
+# Furniture.py - This program calculates profits and sales prices for a furniture company.
+
+itemName = "TV Stand"
+retailPrice = 325.00
+wholesalePrice = 200.00
+profit = retailPrice - wholesalePrice
+salePrice = retailPrice - (retailPrice * .25)
+saleProfit = salePrice - wholesalePrice
+
+
+print("Item Name: " + itemName)
+print("Retail Price: $" + str(retailPrice))
+print("Wholesale Price: $" + str(wholesalePrice))
+print("Profit: $" + str(profit))
+print("Sale Price: $" + str(salePrice))
+print("Sale Profit: $" + str(saleProfit))
